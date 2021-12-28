6cc046a9dbbea6d313d595f0ae44baf3d9e233a9
ghr326 <ghr326@gmail.com>
2021-12-28 09:10:41 +0530

    nvn

---
+++ pywasp.py
@@ -0,0 +1,4 @@
+import pywhatkit
+#pywhatkit.sendwhatmsg("Learning AWS & DevOps","GOOD AFTERNOON PRENDS", 14, 35 )
+#pywhatkit.sendwhatmsg_to_group("Learning AWS & DevOps", "Hey All py script check", 18, 38 )
+pywhatkit.sendwhatmsg_to_group("Learning AWS & DevOps", "Hey All!", 18, 47)
