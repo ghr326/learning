aa4a534805e8512d607fa2576d2936cf2b6d4df9
ghr326 <ghr326@gmail.com>
2021-12-29 00:19:33 +0530

    .

--- pylearnnvnrdy.py
+++ pylearnnvnrdy.py
@@ -1,1 +1,21 @@
-hi s
+#python by telusko i have to complete atleast today to 4 hr class other wise i ma not good at python
+
+
+#it is a generalpurpose language
+
+#interpretod
+#object oriented 
+#high level language
+
+x=2+3
+print(x)
+
+a=print(4/4)
+
+print(a)
+#strings
+#
+#"string" so it will be quoted as string
+print("nani")
+#when we have ' has two meanings we can useone single 
+print("naveen's laptop")
