75a94b840dc66df3652310867b840264294637fa
ghr326 <ghr326@gmail.com>
2022-01-09 02:40:06 +0530

    1

--- pylearnnvnrdy.py
+++ pylearnnvnrdy.py
@@ -7,6 +7,18 @@
 #object oriented 
 #high level language
 
+from os import linesep
+
+from werkzeug.datastructures import ImmutableDict
+
+#arithematic operations
+#as calculations will bedone by BODMAS rule 
+#and these are given by like
+#+is for addition
+#-is for substraction
+#/ is used for dividing 
+#float is used to print the nos in float values
+
 x=2+3
 print(x)
 
@@ -19,3 +31,36 @@ print(a)
 print("nani")
 #when we have ' has two meanings we can useone single 
 print("naveen's laptop")
+print("naveen\'s laptop")
+print("navin"+"navin")
+print("naveen" *10)
+#\n is represented for new lines only
+#we need to specify r infornt of /n to avoid taking input as new lines
+print('\nnaveen')
+print(r'\nnaveen')
+#if you execute this we can see the difference as it is not taking \n as a new line
+
+
+
+#variables inpython
+# if we give one name or letter is assigned so that you can used for printing and using for any other operatins neeeded
+# so if x=2 so we assigned 2 to x as a number o we can use strings aslo can be assigned to any key or variable so  
+#we can user variablees for strings numbers integers and many stuff
+# 
+# 
+# 
+
+x=2
+print(x)
+print(int(x))
+#ARRAY
+y="hemanthkumargaddam"
+print(y)
+#this type is used for printing certain characters for purpose
+
+print(y[1:9])
+#strings in python are Immutable
+print(y[0])="H"
+
+#functions in python
+names =[nsni,dso]
